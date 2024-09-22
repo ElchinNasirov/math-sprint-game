98ae4a2ae47bdfeb0956fd05a4c027f3a631f0ed
ElchinNasirov <nasirov.elchin@gmail.com>
2024-09-21 22:41:33 -0700

    working on script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+// pages/screens
+const splashPage = document.getElementById('splash-page');
+const countdownPage = document.getElementById('countdown-page');
+const gamePage = document.getElementById('game-page');
+const scorePage = document.getElementById('score-page');
+
+// splash page 
+const startForm = document.getElementById('start-form');
+const radioContainers = document.querySelectorAll('.radio-container');
+const radioInputs = document.querySelectorAll('input');
+const bestScores = document.querySelectorAll('.best-score-value');
+
+//countdown page
+const countdown = document.querySelector('.countdown');
